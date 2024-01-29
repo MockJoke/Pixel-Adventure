5f50f6b3b8405f6ec9c3f89901dd0beff63c51aa
Parva <parvadesai404@gmail.com>
2024-01-29 23:08:39 +0530

    [Add] the project files

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private AudioSource finishSound;
+
+    private bool levelCompleted = false;
+
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !levelCompleted)
+        {
+            finishSound.Play();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
+        }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Build a 2D Platformer Game in Unity | Unity Beginner Tutorial
+
+Watch the course here: https://www.youtube.com/playlist?list=PLrnPJCHvNZuCVTz6lvhR81nnaf1a-b67U
+
+![thumbnail part 1](https://user-images.githubusercontent.com/52977034/119971715-9d0e2a80-bfb1-11eb-8938-b9da00b22b1f.png)
