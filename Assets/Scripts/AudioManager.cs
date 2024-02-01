cfedfdf1c19ed2f78a5560479cbd933b44f788d9
Parva <parvadesai404@gmail.com>
2024-02-01 15:43:09 +0530

    [Add] Direct mutation functionality support for AudioManager
    Updated logic for the case when a non-looping bg music completes then automatically start a new one.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,15 +6,14 @@ public class AudioManager : MonoBehaviour
     public Sound[] effectSounds;
     public Sound[] bgSounds;
 
-    private int currBgMusicIndex = -1;      // set high to signify no song playing
-
-    // a play music flag so we can stop playing music during cutscenes etc
-    private bool shouldPlayMusic = false; 
+    private int currBgMusicIndex = -1;      // set -1 to signify no song playing
 
     public static AudioManager instance; // will hold a reference to the first AudioManager created
 
-    private float mvol; // Global music volume
-    private float evol; // Global effects volume
+    private float mVol; // Global music volume
+    private float eVol; // Global effects volume
+    
+    public bool isMuted = false;
     
     void Awake() 
     {
@@ -31,11 +30,11 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         // get preferences
-        mvol = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
-        evol = PlayerPrefs.GetFloat("EffectsVolume", 0.75f);
+        mVol = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        eVol = PlayerPrefs.GetFloat("EffectsVolume", 1f);
 
-        createAudioSources(effectSounds, evol);     // create sources for effects
-        createAudioSources(bgSounds, mvol);   // create sources for music
+        createAudioSources(effectSounds, eVol);     // create sources for effects
+        createAudioSources(bgSounds, mVol);   // create sources for music
     }
     
     void Start() 
@@ -44,22 +43,21 @@ public class AudioManager : MonoBehaviour
         {
             PlayMusic(true);
         }
+        
+        SetMuteState();
     }
     
     void Update() 
     {
-        // if we are playing a track from the playlist && it has stopped playing
-        // if (currentPlayingIndex != 999 && !bgSounds[currentPlayingIndex].source.isPlaying) 
-        // {
-        //     currentPlayingIndex++; // set next index
-        //     
-        //     if (currentPlayingIndex >= bgSounds.Length) 
-        //     { //have we went too high
-        //         currentPlayingIndex = 0; // reset list when max reached
-        //     }
-        //     
-        //     bgSounds[currentPlayingIndex].source.Play(); // play that funky music
-        // }
+        // if we are playing a track (which wasn't on loop) from the playlist
+        if (currBgMusicIndex != -1) 
+        {
+            // and it has stopped playing
+            if (!bgSounds[currBgMusicIndex].source.isPlaying)
+            {
+                PlayMusic(true);
+            }
+        }
     }
     
     // create sources
@@ -101,40 +99,32 @@ public class AudioManager : MonoBehaviour
         s.source.Stop();
     }
     
-    public void PlayMusic(bool random, AudioType audioName = AudioType.None) 
+    public void PlayMusic(bool random, AudioType audioName = AudioType.None)
     {
-        if (shouldPlayMusic == false) 
+        if (random || audioName == AudioType.None)
         {
-            shouldPlayMusic = true;
+            // pick a random song from our playlist
+            currBgMusicIndex = GetRandomBgMusicIndex();
+        }
+        else
+        {
+            currBgMusicIndex = Array.FindIndex(effectSounds, sound => sound.name == audioName);
 
-            if (random)
+            if (currBgMusicIndex < 0)
             {
                 // pick a random song from our playlist
                 currBgMusicIndex = GetRandomBgMusicIndex();
             }
-            else
-            {
-                currBgMusicIndex = Array.FindIndex(effectSounds, sound => sound.name == audioName);
-
-                if (currBgMusicIndex < 0)
-                {
-                    // pick a random song from our playlist
-                    currBgMusicIndex = GetRandomBgMusicIndex();
-                }
-            }
-            
-            bgSounds[currBgMusicIndex].source.Play();
         }
+            
+        bgSounds[currBgMusicIndex].source.Play();
     }
 
     // stop music
-    public void StopMusic() 
+    public void StopMusic()
     {
-        if (shouldPlayMusic) 
-        {
-            shouldPlayMusic = false;
-            currBgMusicIndex = 999; // reset playlist counter
-        }
+        bgSounds[currBgMusicIndex].source.Stop();
+        currBgMusicIndex = -1;
     }
 
     private int GetRandomBgMusicIndex()
@@ -143,32 +133,52 @@ public class AudioManager : MonoBehaviour
     }
     
     // get the song name
-    public AudioType getBgSongName() 
+    public AudioType getCurrBgSongName() 
     {
         return bgSounds[currBgMusicIndex].name;
     }
 
     // if the music volume change update all the audio sources
-    public void musicVolumeChanged()
+    public void musicVolumeChanged(float val)
     {
-        mvol = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        mVol = val;
+        PlayerPrefs.SetFloat("MusicVolume", mVol);
         
         foreach (Sound m in bgSounds) 
         {
-            m.source.volume = m.volume * mvol;
+            m.source.volume = m.volume * mVol;
         }
     }
 
     // if the effects volume changed update the audio sources
-    public void effectVolumeChanged() 
+    public void effectVolumeChanged(float val) 
     {
-        evol = PlayerPrefs.GetFloat("EffectsVolume", 0.75f);
+        eVol = val;
+        PlayerPrefs.SetFloat("EffectsVolume", eVol);
         
         foreach (Sound s in effectSounds) 
         {
-            s.source.volume = s.volume * evol;
+            s.source.volume = s.volume * eVol;
         }
         
         // effectSounds[0].source.Play();
+    }
+    
+    private void SetMuteState()
+    {
+        isMuted = PlayerPrefs.GetInt("Muted") == 1;
+        SetAudioListener();
+    }
+    
+    public void ToggleMuteState()
+    {
+        isMuted = !isMuted;
+        PlayerPrefs.SetInt("Muted", isMuted ? 1 : 0);
+        SetAudioListener();
+    }
+
+    private void SetAudioListener()
+    {
+        AudioListener.pause = isMuted;
     }
 }
