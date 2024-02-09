eecacc469b8b12376494cb348887f58b345f60aa
Parva <parvadesai404@gmail.com>
2024-02-09 14:06:19 +0530

    [Fix] Refactoring

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -6,7 +6,7 @@ public class WaypointFollower : MonoBehaviour
     private int currentWaypointIndex = 0;
 
     [SerializeField] private float speed = 2f;
-
+    
     private void Update()
     {
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
