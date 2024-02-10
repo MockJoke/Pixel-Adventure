1b39b61accddd54bcde1e6d84a639c3f2ec23387
Parva <parvadesai404@gmail.com>
2024-02-10 14:38:13 +0530

    [Update] Functionality to clamp camera movement in vertical direction

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private float minYBound = -3.75f;
+    [SerializeField] private float maxYBound = 5.75f;
 
     void Awake()
     {
@@ -12,6 +14,10 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        var playerPos = player.position;
+
+        playerPos.y = Mathf.Clamp(playerPos.y, minYBound, maxYBound);
+        
+        transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
     }
 }
