74c09c0c0ac2e37d87f3f223d582ebd1c170aee5
Parva <parvadesai404@gmail.com>
2024-02-10 14:39:14 +0530

    [Add] Level Setup for Player & Enemy Damage

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -3,8 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    [SerializeField] private int lifeCount = 3;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator animator;
+    [SerializeField] private BoxCollider2D boxCollider;
     private static readonly int deathAnim = Animator.StringToHash("death");
 
     void Awake()
@@ -14,6 +16,9 @@ public class PlayerLife : MonoBehaviour
         
         if (animator == null)
             animator = GetComponent<Animator>();
+
+        if (boxCollider == null)
+            boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -27,10 +32,24 @@ public class PlayerLife : MonoBehaviour
     private void Die()
     {
         AudioManager.instance.PlaySound(AudioType.characterDeath);
-        rb.bodyType = RigidbodyType2D.Static;
         animator.SetTrigger(deathAnim);
+        boxCollider.enabled = false;
     }
 
+    private void CheckLifeStatus()
+    {
+        RestartLevel();
+        
+        // if (lifeCount >= 0)
+        // {
+        //     RestartLevel();
+        // }
+        // else
+        // {
+        //     SceneManager.LoadScene("Scenes/End Screen");
+        // }
+    }
+    
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
