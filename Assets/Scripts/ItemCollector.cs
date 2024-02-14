adbaa50d131301b6f9ad4cf9008f7ac6eca1dd9c
Parva <parvadesai404@gmail.com>
2024-02-14 14:44:53 +0530

    [Update] Cherries (Item) UI/UX

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -4,7 +4,13 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
+    [SerializeField] private int TotalCherries = 5;
     [SerializeField] private Text cherriesText;
+
+    void Start()
+    {
+        UpdateCherries();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,7 +19,12 @@ public class ItemCollector : MonoBehaviour
             AudioManager.Instance.PlaySound(AudioType.itemCollect);
             Destroy(collision.gameObject);
             cherries++;
-            cherriesText.text = "Cherries: " + cherries;
+            UpdateCherries();
         }
+    }
+
+    private void UpdateCherries()
+    {
+        cherriesText.text = $"{cherries}/{TotalCherries}";
     }
 }
