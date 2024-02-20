1d6efa80fa9152739414b0f63e67a9340cbc38d5
Guru-404 <materialsforteachingstudents@gmail.com>
2024-02-20 18:18:09 +0530

    [Update] Simplify fire trap logic
    Removed trigger & activation logic from fire trap. Fire trap now will be always active.

--- Assets/Scripts/FireTrap.cs
+++ Assets/Scripts/FireTrap.cs
@@ -8,15 +8,15 @@ public class FireTrap : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRend;
     
-    [Header("Firetrap Timers")]
-    [SerializeField] private float activationDelay;
-    [SerializeField] private float activeTime;
+    // [Header("Firetrap Timers")]
+    // [SerializeField] private float activationDelay;
+    // [SerializeField] private float activeTime;
 
-    private bool triggered; //when the trap gets triggered
-    private bool active; //when the trap is active and can hurt the player
+    // private bool triggered;     //when the trap gets triggered
+    // private bool active;        //when the trap is active and can hurt the player
 
-    private PlayerLife playerLife;
-    private static readonly int activatedAnim = Animator.StringToHash("Activated");
+    // private PlayerLife playerLife;
+    // private static readonly int activatedAnim = Animator.StringToHash("Activated");
 
     void Awake()
     {
@@ -29,44 +29,39 @@ public class FireTrap : MonoBehaviour
 
     void Update()
     {
-        if (playerLife != null && active)
-            playerLife.Die();
+        // if (playerLife != null && active)
+        //     playerLife.Die();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (!triggered)
-                StartCoroutine(ActivateFiretrap());
-
-            if (active)
+            // if (!triggered)
+            //     StartCoroutine(ActivateFiretrap());
+            //
+            // if (active)
                 collision.GetComponent<PlayerLife>().Die();
         }
     }
     
-    // private void OnTriggerExit2D(Collider2D collision)
+    // private IEnumerator ActivateFiretrap()
     // {
-    //     if (collision.CompareTag("Player"))
-    //         playerLife = null;
+    //     //turn the sprite red to notify the player and trigger the trap
+    //     triggered = true;
+    //     spriteRend.color = Color.red;
+    //
+    //     //Wait for delay, activate trap, turn on animation, return color back to normal
+    //     yield return new WaitForSeconds(activationDelay);
+    //     
+    //     spriteRend.color = Color.white; //turn the sprite back to its initial color
+    //     active = true;
+    //     animator.SetBool(activatedAnim, true);
+    //
+    //     //Wait until X seconds, deactivate trap and reset all variables and animator
+    //     yield return new WaitForSeconds(activeTime);
+    //     active = false;
+    //     triggered = false;
+    //     animator.SetBool(activatedAnim, false);
     // }
-    
-    private IEnumerator ActivateFiretrap()
-    {
-        //turn the sprite red to notify the player and trigger the trap
-        triggered = true;
-        spriteRend.color = Color.red;
-
-        //Wait for delay, activate trap, turn on animation, return color back to normal
-        yield return new WaitForSeconds(activationDelay);
-        spriteRend.color = Color.white; //turn the sprite back to its initial color
-        active = true;
-        animator.SetBool(activatedAnim, true);
-
-        //Wait until X seconds, deactivate trap and reset all variables and animator
-        yield return new WaitForSeconds(activeTime);
-        active = false;
-        triggered = false;
-        animator.SetBool(activatedAnim, false);
-    }
 }
