d7344defa2203e55e7aae1e1572267aa4f72d0d4
Guru-404 <materialsforteachingstudents@gmail.com>
2024-02-21 18:50:42 +0530

    [Update] Added spring effect to falling platformer

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -2,27 +2,44 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpringJoint2D springJoint;
+    
+    [Header("Falling Parameters")]
     [SerializeField] private float fallDelay = 2.5f;
     [SerializeField] private float destroyDelay = 3f;
-    [SerializeField] private Rigidbody2D rb;
+    
+    private static readonly int fallingAnim = Animator.StringToHash("Falling");
 
     void Awake()
     {
         if (rb == null)
             rb = GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        if (springJoint == null)
+            springJoint = GetComponent<SpringJoint2D>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            springJoint.enabled = true;
+            rb.bodyType = RigidbodyType2D.Dynamic;
             Invoke(nameof(Fall), fallDelay);
         }
     }
 
     private void Fall()
     {
-        rb.bodyType = RigidbodyType2D.Dynamic;
+        springJoint.enabled = false;
+        animator.SetTrigger(fallingAnim);
+        
         Destroy(gameObject, destroyDelay);
     }
 }
