21d310ae8d0950eb086061579834e6c3e901673b
Parva <parvadesai404@gmail.com>
2024-02-22 14:23:49 +0530

    [Update] Added an option to choose direction of rotation in Rotate utility

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,10 +2,31 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    private enum direction
+    {
+        none,
+        clockwise,
+        anticlockwise
+    }
+    
     [SerializeField] private float speed = 2f;
-
+    [SerializeField] private direction rotationDirection = direction.clockwise;
+ 
     void Update()
     {
-        transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
+        switch (rotationDirection)
+        {
+            case direction.clockwise:
+                transform.Rotate(0, 0, -360 * speed * Time.deltaTime);
+                break;
+            case direction.anticlockwise:
+                transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
+                break;
+            case direction.none:
+                break;
+            default:
+                transform.Rotate(0, 0, -360 * speed * Time.deltaTime);
+                break;
+        }
     }
 }
