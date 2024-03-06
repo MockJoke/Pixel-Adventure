93501d66955c7b172f47b92d0443756bf580ac28
Parva <parvadesai404@gmail.com>
2024-03-06 16:05:28 +0530

    [Add] Separate enum for RotationDirection

--- Assets/Scripts/Objects/Rotate.cs
+++ Assets/Scripts/Objects/Rotate.cs
@@ -2,27 +2,20 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    private enum direction
-    {
-        none,
-        clockwise,
-        anticlockwise
-    }
-    
     [SerializeField] private float speed = 2f;
-    [SerializeField] private direction rotationDirection = direction.clockwise;
+    [SerializeField] private RotationDirection rotationDirection = RotationDirection.clockwise;
  
     void Update()
     {
         switch (rotationDirection)
         {
-            case direction.clockwise:
+            case RotationDirection.clockwise:
                 transform.Rotate(0, 0, -360 * speed * Time.deltaTime);
                 break;
-            case direction.anticlockwise:
+            case RotationDirection.anticlockwise:
                 transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
                 break;
-            case direction.none:
+            case RotationDirection.none:
                 break;
             default:
                 transform.Rotate(0, 0, -360 * speed * Time.deltaTime);
---
+++ Assets/Scripts/Objects/RotationDirection.cs
@@ -0,0 +1,7 @@
+
+public enum RotationDirection
+{
+    none,
+    clockwise,
+    anticlockwise
+}
