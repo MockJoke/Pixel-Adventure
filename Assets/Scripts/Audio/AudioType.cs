84a8b2ff8c648c7e2ab8181349c504b44a225d78
Parva <parvadesai404@gmail.com>
2024-03-24 17:17:52 +0530

    [Add] more audio types
    more bg music options, enemy-hit, checkpoint-pass

--- Assets/Scripts/Audio/AudioType.cs
+++ Assets/Scripts/Audio/AudioType.cs
@@ -8,15 +8,23 @@ public enum AudioType
     bg2 = 2,
     bg3 = 3,
     bg4 = 4,
+    bg5 = 5,
+    bg6 = 6,
+    bg7 = 7,
+    bg8 = 8,
+    bg9 = 9,
+    bg10 = 10,
     #endregion
     
     #region Character 11 - 30
     characterJump = 11,
     characterDeath = 12,
+    enemyHit = 13,
     #endregion
 
     #region Collectibles 31 - 40
     levelFinish = 31,
     itemCollect = 32,
+    checkpointPass = 33,
     #endregion
 }
